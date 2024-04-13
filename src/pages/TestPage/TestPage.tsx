7c47461a07ca46329bc47b07c0acbce37187f0d9
SanyaCLAUZ <58825843+SanyaCLAUZ@users.noreply.github.com>
2024-04-13 19:13:49 -0400

    Added stair raiting animation

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./TestPage.css";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
 
 interface Props {}
 
 const TestPage = (props: Props) => {
   return (
     <div>
-      <h1>TestPage</h1>
+      <div>
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+      </div>
+
       <button className="button-13" role="button">
         <Link to="/">Go to Home Page</Link>
       </button>
     </div>
   );
 };
+
+function Star() {
+  const [hidden, setHidden] = useState(false);
+  return (
+    <span
+      className="star_container"
+      onMouseEnter={() => setHidden(true)}
+      onMouseLeave={() => setHidden(false)}
+    >
+      {hidden ? <FaStar /> : <FaRegStar /> }
+    </span>
+  );
+}
 
 export default TestPage;
