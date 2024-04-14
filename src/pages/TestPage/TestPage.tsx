baeb9717ade26f8a525ca815242a2cdea62c8f9c
SanyaCLAUZ <58825843+SanyaCLAUZ@users.noreply.github.com>
2024-04-14 15:54:44 -0400

    Added logic and functionality for stars, split files, start styled-components

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./TestPage.css";
-import { FaStar } from "react-icons/fa";
-import { FaRegStar } from "react-icons/fa";
+import StarContainer from "../../Components/Star/StarContainer";
+import Accordion from "../../Components/Accordion/Accordion";
 
 interface Props {}
 
@@ -10,31 +10,16 @@ const TestPage = (props: Props) => {
   return (
     <div>
       <div>
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <StarContainer />
       </div>
-
+      <div>
+        <Accordion titleValue={"Title"}  bodyValue={"Body"}/>
+      </div>
       <button className="button-13" role="button">
         <Link to="/">Go to Home Page</Link>
       </button>
     </div>
   );
 };
-
-function Star() {
-  const [hidden, setHidden] = useState(false);
-  return (
-    <span
-      className="star_container"
-      onMouseEnter={() => setHidden(true)}
-      onMouseLeave={() => setHidden(false)}
-    >
-      {hidden ? <FaStar /> : <FaRegStar /> }
-    </span>
-  );
-}
 
 export default TestPage;
